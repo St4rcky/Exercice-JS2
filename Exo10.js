a974b9a956997b08cbc50046ffdeabe49fac8030
St4rcky <pascal.starck@epsi.fr>
2024-02-13 14:33:05 +0100

    10

--- Exo10.js
+++ Exo10.js
@@ -1,15 +1,30 @@
 let ajouter = document.querySelector("button");
 let listes = document.querySelector("ul");
+let deleteAll = document.getElementById("deleteAll");
+let storage = JSON.parse(localStorage.getItem("taches")) || [];
 
 ajouter.addEventListener("click", function (e) {
   e.preventDefault();
-  let taches = document.querySelector("input").value;
+  let taches = document.getElementById("nomDeTache").value;
   let liste = document.createElement("li");
   let texte = document.createElement("p");
   texte.textContent = taches;
   listes.appendChild(liste);
   liste.style.display = "flex";
   liste.style.alignItems = "center";
+
+  let texteContainer = document.createElement("div");
+  liste.appendChild(texteContainer);
+
+  liste.addEventListener("dblclick", function (e) {
+    let input = document.createElement("input");
+    input.type = "text";
+    input.value = liste.textContent;
+    texte.appendChild(input);
+    input.addEventListener("blur", function () {
+      texte.textContent = input.value;
+    });
+  });
 
   let caseACocher = document.createElement("input");
   caseACocher.type = "checkbox";
@@ -22,21 +37,21 @@ ajouter.addEventListener("click", function (e) {
       liste.style.color = "black";
     }
   });
+  document.getElementById("nomDeTache").value = "";
+  let deleteButton = document.createElement("button");
+  deleteButton.textContent = `\u00D7`;
+  liste.appendChild(deleteButton);
+
+  deleteButton.addEventListener("click", function () {
+    liste.remove();
+  });
+
+  storage.push(taches);
+  localStorage.setItem("taches", JSON.stringify(storage));
 });
 
-// ------- Code avant de mettre la case en place, il fonctionne bien ---------
-
-// let ajouter = document.querySelector("button");
-// let listes = document.querySelector("ul");
-
-// ajouter.addEventListener("click", function (e) {
-//   e.preventDefault();
-//   let taches = document.querySelector("input").value;
-//   let liste = document.createElement("li");
-//   liste.textContent = taches;
-//   listes.appendChild(liste);
-
-//   liste.addEventListener("click", function () {
-//     liste.style.color = "green";
-//   });
-// });
+deleteAll.addEventListener("click", function () {
+  listes.innerHTML = "";
+  localStorage.removeItem("taches");
+  storage = [];
+});
